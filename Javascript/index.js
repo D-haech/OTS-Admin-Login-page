a7f657e607fd42157273261d5bfb373582fea089
D-haech <osarogiedanieligbinosa@gmail.com>
2024-04-19 16:07:27 +0100

    About to start Javascript

---
+++ Javascript/index.js
@@ -0,0 +1,40 @@
+//Declaring needed variables
+const menuButton = document.querySelector(".menu");
+const sidebar = document.querySelector(".sidebar");
+const login = document.querySelector('.login');
+const mainDashboard = window.document.querySelector(".main-dashboard");
+const sideDashboard = document.querySelector('.dashboard');
+let switchOn = true;
+
+//Logic starts here
+
+//displays the sidebar
+// menuButton.onClick = () => {
+//   if (switchOn == true) {
+//     sidebar.style.display = "block";
+//     switchOn = false;
+//   } else {
+//     sidebar.style.display = "none";
+//     switchOn = true;
+//   }
+
+// };
+
+
+
+
+const button = document.querySelector('.jswe');
+const inputValue  = document.querySelector('.test');
+function workClick(){
+    console.log("its back");
+}
+
+button.addEventListener("click", workClick());
+
+
+
+// //change the pages
+// sideDashboard.onClicick = () => {
+// sideDashboard.style.border = "2px solid red";
+// }
+
