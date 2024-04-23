dbaea536d4e35adb20d648bc814dd8238d876c71
D-haech <osarogiedanieligbinosa@gmail.com>
2024-04-23 08:46:22 +0100

    Solo Project designed and working

--- Javascript/index.js
+++ Javascript/index.js
@@ -7,6 +7,8 @@ const mainDashboard = window.document.querySelector(".main-dashboard");
 const paymentPage = document.querySelector(".payments");
 const threeDots = document.querySelectorAll(".bi-three-dots-vertical");
 const popUp = document.querySelector(".pop-up");
+const soloProjects = document.querySelector(".solo-projects");
+const soloprojectSidebar = document.querySelector("#solo-project");
 
 // variables for sidebar panel
 const dashboard = document.querySelector("#dashboard");
@@ -28,19 +30,45 @@ menuButton.addEventListener("click", () => {
 //click the Dashboard
 dashboard.addEventListener("click", () => {
   paymentSidebar.classList.remove("side-highlight");
+  soloprojectSidebar.classList.remove('side-highlight');
   dashboard.classList.add("side-highlight");
   mainDashboard.style.display = "block";
-  paymentPage.style.display = "none";
-});
-// Click the payment section
 
+  //switch off other dashboard
+  paymentPage.style.display = "none";
+  soloProjects.style.display = 'none';
+  
+});
+
+
+// Click the payment section
 paymentSidebar.addEventListener("click", () => {
   dashboard.classList.remove("side-highlight");
   paymentSidebar.classList.add("side-highlight");
-  mainDashboard.style.display = "none";
+  soloprojectSidebar.classList.remove('side-highlight');
+  // switch on payment dashboard
   paymentPage.style.display = "block";
+
+  // switch off other dashboard
+  mainDashboard.style.display = "none";
+  soloProjects.style.display = 'none';
+  
 });
 
+
+//Solo project bar clicked
+soloprojectSidebar.addEventListener("click", () => {
+  dashboard.classList.remove("side-highlight");
+  paymentSidebar.classList.remove("side-highlight");
+  soloprojectSidebar.classList.add('side-highlight');
+
+  // switch off other dashboard
+  mainDashboard.style.display = "none";
+  paymentPage.style.display = "none";
+  // switch on the solo project
+  soloProjects.style.display = 'block';
+
+});
 // three dots been clicked
 /*
 threeDots.forEach((e) => {
@@ -48,5 +76,3 @@ threeDots.forEach((e) => {
     popUp.style.display = "block";
   });
 });*/
-
-
